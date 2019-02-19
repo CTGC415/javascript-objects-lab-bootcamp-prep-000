6d1ae856ffdc3297314849d24e4c5da42cecfc31
Cully Curwen <c.t.g.curwen@gmail.com>
2019-02-19 16:51:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,4 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, { [key]: value })
 }
+
